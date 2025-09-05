4ab8c9bae55cc2f62ae964bb60b364f3388dd5de
karchx <kenethra.74@gmail.com>
2025-09-05 13:40:42 -0600

    feat: add go program manage cli

---
+++ go.mod
@@ -0,0 +1,45 @@
+module github.com/karchx/kernel-setup
+
+go 1.24.2
+
+require (
+	github.com/docker/docker v28.4.0+incompatible
+	github.com/gothew/l-og v1.0.1
+	github.com/moby/term v0.5.2
+)
+
+require (
+	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/charmbracelet/lipgloss v0.6.0 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.6.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/sys/atomicwriter v0.1.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220204035834-5ac8409525e0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.63.0 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.38.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
+	gotest.tools/v3 v3.5.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/docker/docker/api/types/build"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	log "github.com/gothew/l-og"
+	"github.com/moby/term"
+	// "github.com/docker/docker/pkg/stdcopy"
+)
+
+func listImage(ctx context.Context, cli *client.Client) ([]image.Summary, error) {
+	return cli.ImageList(ctx, image.ListOptions{})
+}
+
+func pullImage(ctx context.Context, cli *client.Client) (io.ReadCloser, error) {
+	reader, err := cli.ImagePull(ctx, "archlinux", image.PullOptions{
+		Platform: "linux/amd64",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	defer reader.Close()
+	// cli.ImagePull is asynchronous.
+	// The reader needs to be read completely for the pull operation to complete.
+	// If stdout is not required, consider using io.Discard instead of os.Stdout.
+	io.Copy(os.Stdout, reader)
+	return reader, nil
+}
+
+func createContainer(ctx context.Context, cli *client.Client) (io.ReadCloser, error) {
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"echo", "hello world"},
+		Tty:   false,
+	}, nil, nil, nil, "")
+	if err != nil {
+		return nil, err
+	}
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		return nil, err
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, container.LogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+	return out, nil
+}
+
+func createContainerTTY(ctx context.Context, config *container.Config, cli *client.Client) {
+	resp, err := cli.ContainerCreate(ctx, config, nil, nil, nil, "")
+
+	if resp.ID == "" {
+		if _, err := pullImage(ctx, cli); err != nil {
+			log.Error(err)
+			panic(err)
+		}
+	}
+
+	if err != nil {
+		log.Error(err)
+		panic(err)
+	}
+
+	// attach terminal
+	hijack, err := cli.ContainerAttach(ctx, resp.ID, container.AttachOptions{
+		Stream: true, Stdin: true, Stdout: true, Stderr: true, Logs: true,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer hijack.Close()
+
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		panic(err)
+	}
+
+	inFd, _ := term.GetFdInfo(os.Stdin)
+	state, err := term.SetRawTerminal(inFd)
+	if err != nil {
+		panic(err)
+	}
+
+	defer term.RestoreTerminal(inFd, state)
+
+	go func() {
+		_, _ = io.Copy(hijack.Conn, os.Stdin)
+	}()
+
+	_, _ = io.Copy(os.Stdout, hijack.Conn)
+}
+
+func buildDockerfile(ctx context.Context, dockerfile string, cli *client.Client) {
+	tarBuf := new(bytes.Buffer)
+	tw := tar.NewWriter(tarBuf)
+
+	file, err := os.Open(dockerfile)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	stat, _ := file.Stat()
+	hdr := &tar.Header{
+		Name: filepath.Base(dockerfile),
+		Mode: 0600,
+		Size: stat.Size(),
+	}
+	if err := tw.WriteHeader(hdr); err != nil {
+		panic(err)
+	}
+
+	if _, err := io.Copy(tw, file); err != nil {
+		panic(err)
+	}
+	tw.Close()
+
+	buildResp, err := cli.ImageBuild(ctx, tarBuf, build.ImageBuildOptions{
+		Tags:       []string{"lab:latest"},
+		Dockerfile: filepath.Base(dockerfile),
+		Remove:     true,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	defer buildResp.Body.Close()
+	_, _ = io.Copy(os.Stdout, buildResp.Body)
+	log.Info("Build complete")
+}
+
+func main() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	dockerfile := "dockers/Dockerfile.work"
+	buildDockerfile(ctx, dockerfile, cli)
+
+	// createContainerTTY(ctx, &container.Config{
+	// 	Image:        "archlinux",
+	// 	Cmd:          []string{"/bin/sh"},
+	// 	Tty:          true,
+	// 	OpenStdin:    true,
+	// 	AttachStdin:  true,
+	// 	AttachStdout: true,
+	// 	AttachStderr: true,
+	// }, cli)
+	// stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+}
